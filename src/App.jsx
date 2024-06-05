b8c42c467b512fe45eac137044174627020da5a7
christinasamson <156719832+christinasamson@users.noreply.github.com>
2024-06-05 21:42:47 +0200

    Added header.jsx and Layout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,27 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Vans from '../pages/Vans';
+import VansDetail from '../pages/VansDetail';
+import Layout from '../components/Layout';
+
+import "/server"
+
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VansDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
 }
+
 
 ReactDOM
   .createRoot(document.getElementById('root'))
