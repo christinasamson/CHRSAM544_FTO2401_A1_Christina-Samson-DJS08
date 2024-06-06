7d1d63a3e94e77de8a4fa2e4a4158a8927cffdc5
christinasamson <156719832+christinasamson@users.noreply.github.com>
2024-06-06 21:19:32 +0200

    added new files and modified vans.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,14 @@ import VansDetail from '../pages/Vans/VansDetail';
 import Dashboard from '../pages/Host/Dashboard'; 
 import Income from '../pages/Host/Income';
 import Reviews from '../pages/Host/Reviews';
+import HostVans from '../pages/Host/HostVans.jsx';
+import HostVanDetails from '../pages/Host/HostVanDetails.jsx';
+import HostVanPricing from '../pages/Host/HostVanPricing';
+import HostVanInfo from '../pages/Host/HostVanInfo.jsx';
+import HostVanPhotos from '../pages/Host/HostVanPhotos.jsx';
 import Layout from '../components/Layout';
 import HostLayout from '../components/HostLayout';
+
 import "/server"
 
 
@@ -27,12 +33,19 @@ export default function App() {
             <Route path="/host" element={<Dashboard />} />
             <Route path="/host/income" element={<Income />} />
             <Route path="/host/reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetails />}>
+              <Route index element={<HostVanInfo/>} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
     </BrowserRouter>
   )
 }
+
 
 ReactDOM
   .createRoot(document.getElementById('root'))
